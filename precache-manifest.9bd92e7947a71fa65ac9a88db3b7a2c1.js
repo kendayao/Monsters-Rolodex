d72586f2e96e6be33906d8e0fbdb5e0fd63357ec
Kaeneth Dayao <kendayao@gmail.com>
2020-09-29 14:41:54 -0700

    Updates

---
+++ precache-manifest.9bd92e7947a71fa65ac9a88db3b7a2c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03335d1b3ae6be439a197aa5d6a695d6",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "19b8201a027a1a20a312",
+    "url": "/Monsters-Rolodex/static/css/main.5952373f.chunk.css"
+  },
+  {
+    "revision": "ddfc98fd1fb7cc1baeab",
+    "url": "/Monsters-Rolodex/static/js/2.fcb60162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.fcb60162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b8201a027a1a20a312",
+    "url": "/Monsters-Rolodex/static/js/main.e6484442.chunk.js"
+  },
+  {
+    "revision": "a94e0472f750e6c9a941",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.c31a7727.js"
+  }
+]);
